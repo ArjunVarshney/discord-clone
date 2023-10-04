a85cacdec4fdfd6b37fefdc2bb951da0626613dc
Arjun Varshney <varshneyarjun49@gmail.com>
2023-10-04 06:18:13 +0530

    configured uploadthing

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
    name: z.string().min(1, "Server name is required"),
@@ -72,7 +73,21 @@ const InitialModal = () => {
                >
                   <div className="space-y-8 px-6">
                      <div className="flex items-center justify-center text-center">
-                        Image Upload
+                        <FormField
+                           control={form.control}
+                           name="imageUrl"
+                           render={({ field }) => (
+                              <FormItem>
+                                 <FormControl>
+                                    <FileUpload
+                                       endpoint="serverImage"
+                                       value={field.value}
+                                       onChange={field.onChange}
+                                    />
+                                 </FormControl>
+                              </FormItem>
+                           )}
+                        />
                      </div>
                      <FormField
                         control={form.control}
